3758c135823bf8e08540cce002092e5021eddf8c
Miguel <mlopez@mlopez.(none)>
2013-12-31 02:48:35 -0600

    Avance en Angular, CRUD de UserAdmin en Scala/Play

--- AngularJS/AngularFundamentals/app/js/controllers/EventController.js
+++ AngularJS/AngularFundamentals/app/js/controllers/EventController.js
@@ -3,19 +3,24 @@
 eventsApp.controller('EventController', function EventController($scope, eventData) {
     $scope.snippet = "<span style='color:red'>hi there</span>";
     $scope.boolValue = false;
-    $scope.myStyle = { color: 'red' }
+    $scope.myStyle = { color: 'red' };
     $scope.myClass = 'blue';
     $scope.buttonDisabled = true;
     $scope.sortorder = 'name';
-    $scope.event = eventData;
     
-    $scope.upVoteSession = function(session) {
+    $scope.event = eventData.event;
+    $scope.event.then(
+        function (event) { console.log(event); },
+        function (response) { console.log(response); } 
+    );
+    
+    $scope.upVoteSession = function (session) {
         session.upVoteCount++;
-    }
+    };
     
-    $scope.downVoteSession = function(session) {
+    $scope.downVoteSession = function (session) {
         session.upVoteCount--;
-    }
+    };
     
 });
                      
--- AngularJS/AngularFundamentals/app/js/controllers/SaveEventController.js
+++ AngularJS/AngularFundamentals/app/js/controllers/SaveEventController.js
@@ -1,17 +1,24 @@
 'use strict';
 
-eventsApp.controller('SaveEventController', function SaveEventController($scope) {
+eventsApp.controller('SaveEventController', function SaveEventController($scope, eventData) {
 
-    $scope.saveEvent = function(event, saveEventForm) {
-        console.log(saveEventForm);
-        if(saveEventForm.$valid) {
-            alert("valid form!");
+        $scope.saveEvent = function (event, form) {
+            console.log(form);
+            if (form.$valid) {
+                eventData.save(event)
+                    .then(
+                        function (response) {
+                            console.log('success', response);
+                        },
+                        function (response) {
+                            console.log('failure', response);
+                        }
+                );
+            }
+        };
+
+        $scope.cancelSave = function () {
+            window.location = "/app/eventDetails.html";
         }
-    };
-    
-    $scope.cancelSave = function() {
-        window.location = "/app/eventDetails.html";   
     }
-    
 });
-                     
