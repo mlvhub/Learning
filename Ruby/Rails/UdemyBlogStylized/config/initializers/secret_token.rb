c6b5e110d880f2cebf5bca383b230f68b844abb9
Miguel <miguellova@gmail.com>
2014-02-22 23:29:23 -0600

    Lesson 4 of Udemy Course

---
+++ Ruby/Rails/UdemyBlogStylized/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UdemyBlogStylized::Application.config.secret_key_base = '79312b6799690d1203e775cbd5c0e7f9192f29675602f025b7a2a041938450c2743c9545c53dca106cea9a71f5691f25c1f1de081203a16f76f62bf5a3d43148'
