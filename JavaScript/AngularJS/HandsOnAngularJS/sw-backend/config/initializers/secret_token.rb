15759f268d4b5b37cc7977f964459ab06f47c8ff
Miguel <miguellova@gmail.com>
2014-04-18 21:48:12 -0600

    Done with HandsOnAngular 3, created rails project for the backend, and configuration of a proxy in frontend to call the backend's api

---
+++ JavaScript/AngularJS/HandsOnAngularJS/sw-backend/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SwBackend::Application.config.secret_key_base = '068d16bb378cb9f3e2a6de1abc47f28cc823c70d59bc0bb8a78cbfce25c3d572fe19d1e9076b666c96c4cd490a46a6bdb06243b3be707f2a2d83ae459b20c08a'
