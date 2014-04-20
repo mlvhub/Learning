9cb4ba5867233229ddeebf81e2b5bf4392093523
Miguel <miguellova@gmail.com>
2014-04-20 17:32:36 -0600

    MakeItSnappy - 3-3 - Now we show and paginate unsolved questions

--- Ruby/Rails/MakeItSnappy/app/controllers/questions_controller.rb
+++ Ruby/Rails/MakeItSnappy/app/controllers/questions_controller.rb
@@ -2,6 +2,7 @@ class QuestionsController < ApplicationController
   before_filter :auth, only: [:create]
   def index
     @question = Question.new
+    @questions = Question.unsolved(params)
   end
 
   def create
@@ -10,6 +11,7 @@ class QuestionsController < ApplicationController
       flash[:success] = "Your question has been posted"
       redirect_to root_url
     else
+      @questions = Question.unsolved(params)
       render 'index'
     end
   end
