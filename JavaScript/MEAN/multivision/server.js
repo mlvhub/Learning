0c93f68afeb929ee9c50471533dc3d0842f5ced6
Miguel <miguellova@gmail.com>
2014-05-09 23:09:03 -0600

    Done with Adding Styles - MEAN 2-6

--- JavaScript/MEAN/multivision/server.js
+++ JavaScript/MEAN/multivision/server.js
@@ -1,11 +1,26 @@
-var express = require("express");
+var express = require("express"),
+    stylus = require('stylus'),
+    logger = require('morgan');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+function compile(str, path) {
+  return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger);
+app.use(stylus.middleware(
+      {
+        src: __dirname + '/public',
+        compile: compile
+      }
+));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
   res.render('index');
