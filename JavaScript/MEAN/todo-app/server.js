45bba21a29cfa04c687fa1bacdcb5d2e9f4bf2f1
Miguel <miguellova@gmail.com>
2014-05-11 23:23:38 -0600

    Started SPA with Node and Angular tutorial at Scotch.io, done with backend

---
+++ JavaScript/MEAN/todo-app/server.js
@@ -0,0 +1,58 @@
+var express = require('express'),
+    app = express(),
+    mongoose = require('mongoose'),
+    logger = require('morgan');
+
+mongoose.connect('mongodb://localhost/todo');
+
+app.use(express.static(__dirname + '/public'));
+app.use(logger());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+var Todo = mongoose.model('Todo', {
+  text: String
+});
+
+// Routes
+app.get('/api/todos', function (req, res) {
+  Todo.find(function(err, todos) {
+    if (err)
+      res.send(err);
+    
+    res.json(todos);
+  });
+});
+
+app.post('/api/todos', function(req, res) {
+  Todo.create({
+    text: req.body.text,
+    done: false
+  }, function(err, todo) {
+      if(err)
+        res.send(err);
+
+      Todo.find(function(err, todos) {
+        if(err)
+          res.send(err);
+        
+        res.json(todos);
+      });
+  });
+});
+
+app.delete('/api/todos/:todo_id', function(req, res){
+  Todo.remove({
+    _id: req.params.todo_id
+  }, function(err, todo) {
+    if(err)
+      res.send(err);
+    
+    Todo.find(function(err, todos) {
+      res.json(todos);
+    });
+  });
+});
+
+app.listen(3030);
+console.log('App listening on port 3030');
