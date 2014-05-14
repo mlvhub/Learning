11d21613325e11e26fdb6da02b7e420c901a5eda
Miguel <miguellova@gmail.com>
2014-05-13 21:28:41 -0600

    Did the TDD practice in NodeCraftsman book

---
+++ JavaScript/NodeCraftsmanBook/NodeTDD/src/greet.js
@@ -0,0 +1,10 @@
+/*jslint node: true */
+'use strict';
+
+var greet = function (name) {
+  if(name === undefined)
+    return "Hello World!";
+  return "Hello " + name + "!";
+};
+
+module.exports = greet;
