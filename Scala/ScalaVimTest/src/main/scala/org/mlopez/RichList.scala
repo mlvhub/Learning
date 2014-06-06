156294efc13ebfd39bae21bfc1a9c1805a29ccd2
Miguel <miguellova@gmail.com>
2014-06-05 22:39:06 -0600

    Did the Django Rest Framework Quickstart tutorial

---
+++ Scala/ScalaVimTest/src/main/scala/org/mlopez/RichList.scala
@@ -0,0 +1,14 @@
+package org.mlopez
+
+object ListHelper {
+
+  def pack[T](xs: List[T]): List[List[T]] = xs match {
+    case Nil => Nil
+    case x :: xs1 =>
+      val (first, rest) = xs span (y => y == x)
+      first :: pack(rest)
+  }
+
+  def encode[T](xs: List[T]): List[(T, Int)] = 
+    pack(xs) map { packed => (packed.head, packed.length) }
+}
