d91d72d58a9fc7c586965de113e4564a6f216e45
Miguel <miguellova@gmail.com>
2014-07-02 21:48:47 -0600

    Done with 12 - 23 Zenva Runner

--- JavaScript/html5dev/zenva-phaser/js/main.js
+++ JavaScript/html5dev/zenva-phaser/js/main.js
@@ -8,5 +8,6 @@ var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, '');
 game.state.add('Boot', ZenvaRunner.Boot);
 game.state.add('Preload', ZenvaRunner.Preload);
 game.state.add('MainMenu', ZenvaRunner.MainMenu);
+game.state.add('Game', ZenvaRunner.Game);
 
 game.state.start('Boot');
