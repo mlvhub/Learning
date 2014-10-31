00b6f3b2e4e43aee4680f7b8085458855918e6a9
Miguel Lopez V <miguel@firstfactory.com>
2014-10-30 20:19:21 -0600

    Progress in the edx haskell course
    Done with Homework and Lab 3 so far.

---
+++ Haskell/edxCourse/test.hs
@@ -0,0 +1,15 @@
+double x = x + x
+
+quadruple x = double (double x)
+
+factorial n = product [1..n]
+
+average ns = sum ns `div` length ns
+
+n = a `div` length xs
+  where a = 10
+        xs = [1, 2, 3, 4, 5]
+
+xs1 = [1,2,3,4,5]
+myLast1 xs = drop (length xs - 1) xs
+myLast2 xs = head (drop (length xs - 1) xs)
